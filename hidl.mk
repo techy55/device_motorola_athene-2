524e337891b9b9584718814e0e2c92084e051677
Prabhjot Singh <prabhjot.sookshminfo@gmail.com>
2019-12-29 17:15:16 +0530

    athene: Enabling sound trigger V2_2

--- hidl.mk
+++ hidl.mk
@@ -16,8 +16,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
 		android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     android.hardware.broadcastradio@1.0-impl
 
 # Camera
