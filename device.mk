fe1f6e175f8038db5ffffd8dd238bbfa9b16921a
Prabhjot Singh <prabhjot.sookshminfo@gmail.com>
2019-12-29 16:38:14 +0530

    athene: Add BoringSSL compatibility library

--- device.mk
+++ device.mk
@@ -154,7 +154,9 @@ PRODUCT_PACKAGES += \
     gralloc.msm8952 \
     copybit.msm8952 \
     hwcomposer.msm8952 \
-    memtrack.msm8952
+    memtrack.msm8952 \
+		liboverlay \
+    libboringssl-compat
 
 # Ebtables
 PRODUCT_PACKAGES += \
