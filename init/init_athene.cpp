427f19cbb43dc19001a7c488164477ca969e1ec0
resurrected21 <prabhjotsingh2323@gmail.com>
2019-12-29 14:56:11 +0000

    fix init

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -50,7 +50,7 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_triple(char const product_prop[], char const vendor_prop[], char const value[])
+void property_override_triple(char const product_prop[], char const system_prop[], char const vendor_prop[], char const value[])
 {
     property_override(product_prop, value);
     property_override(system_prop, value);
